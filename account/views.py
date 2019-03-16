2b916a60ccaf09fb5c72e2f27b923debe650e742
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-17 02:36:15 +0300

    chore: add messaging to the app

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib import messages
 
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
@@ -35,4 +36,7 @@ def edit(request):
 		if user_form.is_valid() and profile_form.is_valid():
 			user_form.save()
 			profile_form.save()
+			messages.success(request, 'Profile updated successfully')
+		else:
+			messages.error(request, 'Error updating your profile')
 	return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
