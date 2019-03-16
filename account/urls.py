ce988a1007cc58e042cb69c9a30b120132389fe1
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-17 01:44:34 +0300

    chore: add register url

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import dashboard
+from .views import dashboard, register
 
 urlpatterns = [
+	path('register/', register, name='register'),
 	path('', include('django.contrib.auth.urls')),
 	path('', dashboard, name='dashboard'),
 ]
