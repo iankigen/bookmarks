c03cb32c6fc5cbaecc4152ca0f54b210754312a8
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-17 01:13:05 +0300

    chore: add dashboard view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+	return render(request, 'account/dashboard.html', {'section': 'Dashboard'})
