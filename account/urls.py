6df95cbae9920809f3b1078f0da5f3587f520034
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-17 02:24:29 +0300

    chore: add profile update view

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import dashboard, register
+from .views import dashboard, register, edit
 
 urlpatterns = [
 	path('register/', register, name='register'),
+	path('edit/', edit, name='edit'),
 	path('', include('django.contrib.auth.urls')),
 	path('', dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 @login_required
@@ -18,6 +19,20 @@ def register(request):
 			new_user = user_form.save(commit=False)
 			new_user.set_password(cd['password'])
 			new_user.save()
+			Profile.objects.create(user=new_user)
 			return render(request, 'account/register_done.html', {'new_user': new_user})
 
 	return render(request, 'account/register.html', {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+	user_form = UserEditForm(instance=request.user)
+	profile_form = ProfileEditForm(instance=request.user)
+	if request.method == 'POST':
+		user_form = UserEditForm(instance=request.user, data=request.POST)
+		profile_form = ProfileEditForm(instance=request.user, data=request.POST, files=request.FILES)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+	return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
