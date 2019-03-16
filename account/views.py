aa3bc23cf0f9b7dedb053d235ce8682472c36f8b
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-17 01:44:47 +0300

    chore: add register view

--- account/views.py
+++ account/views.py
@@ -1,7 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .forms import UserRegistrationForm
+
 
 @login_required
 def dashboard(request):
 	return render(request, 'account/dashboard.html', {'section': 'Dashboard'})
+
+
+def register(request):
+	user_form = UserRegistrationForm()
+	if request.method == 'POST':
+		user_form = UserRegistrationForm(request.POST)
+		if user_form.is_valid():
+			cd = user_form.cleaned_data
+			new_user = user_form.save(commit=False)
+			new_user.set_password(cd['password'])
+			new_user.save()
+			return render(request, 'account/register_done.html', {'new_user': new_user})
+
+	return render(request, 'account/register.html', {'user_form': user_form})
