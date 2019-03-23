141f21cba68b79d2d1e3237330240b9d3b96fc31
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-24 00:21:57 +0300

    chore: add image like url

--- images/urls.py
+++ images/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import image_create, image_detail
+from .views import image_create, image_detail, image_like
 
 app_name = 'images'
 
 urlpatterns = [
 	path('create/', image_create, name='create'),
-	path('detail/<id>/<slug>/', image_detail, name='detail')
+	path('detail/<id>/<slug>/', image_detail, name='detail'),
+	path('like/', image_like, name='like'),
 ]
