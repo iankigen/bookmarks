fd4a0be3a1e8c82680a0eb444dfa6031575c8614
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-24 00:21:50 +0300

    chore: add image like view

--- images/views.py
+++ images/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .forms import ImageForm
 from .models import Image
@@ -25,3 +27,21 @@ def image_create(request):
 def image_detail(request, id, slug):
 	image = get_object_or_404(Image, slug=slug, id=id)
 	return render(request, 'images/image/detail.html', {'image': image})
+
+
+@require_POST
+@login_required
+def image_like(request):
+	image_id = request.POST.get('id', '')
+	action = request.POST.get('action', '')
+	if image_id and action:
+		try:
+			image = Image.objects.get(id=image_id)
+			if action == 'like':
+				image.users_like.add(request.user)
+			else:
+				image.users_like.remove(request.user)
+			return JsonResponse({'status': 'ok'})
+		except:
+			pass
+	return JsonResponse({'status': 'fail'})
