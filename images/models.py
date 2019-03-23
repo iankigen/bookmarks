77fe2ba2d9b94c610b9d37bff7f363d8d7661c63
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-03-24 00:05:30 +0300

    chore: add get image absolute url

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 from django.utils.text import slugify
 
 
@@ -17,6 +18,9 @@ class Image(models.Model):
 		if not self.slug:
 			self.slug = slugify(self.title)
 		super(Image, self).save(*args, **kwargs)
+
+	def get_absolute_url(self):
+		return reverse('images:detail', kwargs={'id': self.id, 'slug': self.slug})
 
 	def __str__(self):
 		return self.title
