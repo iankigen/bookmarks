374a303f38c46c10cb5af98ea825d755369c8485
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-11-13 16:03:12 +0300

    chore: add graphql url

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from graphene_django.views import GraphQLView
+from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('images/', include('images.urls', namespace='images'))
+    path('images/', include('images.urls', namespace='images')),
+    path('graphql/', GraphQLView.as_view(schema=schema, graphiql=True))
 ]
 
 if settings.DEBUG:
